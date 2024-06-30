c1cb09f315201aee34506e7bd2d81a4ba709edf2
AshParker19 <ashparker4219@gmail.com>
2024-06-30 14:25:47 +0100

    turns out I need a src folder for IntelliJ to work. now everything is finally configured. ready to start writing code

---
+++ src/pushswap/main/Main.kt
@@ -0,0 +1,13 @@
+package pushswap.main
+
+import pushswap.models.*
+/*
+        Use in my code:
+                * require and check, maybe assert
+
+ */
+
+
+fun main() {
+        val store = Store()
+}
